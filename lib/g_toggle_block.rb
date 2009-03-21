153421607ef45e18dfbbbb3b28fd290492343c37
Jason Harrelson <jason@lookforwardenterprises.com>
2009-03-21 15:36:56 -0500

    Fixed bug where view_helpers file not included.

--- lib/g_toggle_block.rb
+++ lib/g_toggle_block.rb
@@ -1,8 +1,10 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'g_toggle_block/view_helpers'
+
 module GToggleBlock
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 end
 
 if defined? ActionView::Base
